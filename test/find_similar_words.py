d1b255a8d1e050ef5604588f3af03896d340a64a
LZandbergen <lzandbergen@outlook.com>
2023-04-18 11:43:00 +0200

    Creating term count documents

--- test/find_similar_words.py
+++ test/find_similar_words.py
@@ -1,9 +1,26 @@
+import wordfreq
+import json
+import pysrt as srt
+import re
 import spacy
 
-def get_similar_words(word):
-    nlp = spacy.load('en_core_web_sm')
-    word_token = nlp(word)
-    similar_words = [w.text for w in word_token.similar_by_word()]
-    return similar_words
+sub_filename = "/home/lzandbergen/Documents/NML/LangFlix/test/La.casa.de.papel.S01E01.WEBRip.Netflix.srt"
+nlp = spacy.load('es_core_news_sm')
+LANGUAGE = 'es'
 
-doc = nlp("Hi, my name is Jack. Do you like my car")
+sub_file = srt.open(sub_filename)
+word_dict = {}
+
+for sub in sub_file:
+    expression = re.compile("[\(\<].*?[\)\>]")
+    en_text = expression.sub("", sub.text)
+    doc = nlp(en_text)
+    for word in doc:
+        if len(word.text) > 1:
+            freq = wordfreq.word_frequency(word.text, 'es')
+            zipf = wordfreq.zipf_frequency(word.text, 'es')
+            word_dict[word.text.lower()] = [freq, zipf]
+
+json = json.dumps(word_dict)
+with open(sub_filename + "dict", "w") as f:
+    f.write(json)
