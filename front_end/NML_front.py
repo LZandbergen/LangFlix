06e51d75304d066b60b809a4fd2f9f124d94b316
Azidalus <brueva.vi@gmail.com>
2023-04-19 14:31:14 +0200

    Update NML_front.py

--- front_end/NML_front.py
+++ front_end/NML_front.py
@@ -44,10 +44,36 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QtCore.QSize(600, 300))
         self.setStyleSheet("""background-color: #4B4948;""")
         
-        
+        # Create a top-level layout
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        # Create and connect the combo box to switch between pages
+        self.pageCombo = QtWidgets.QPushButton("Dictionary")
+        self.pageCombo.addItems(["Page 1", "Page 2"])
+        self.pageCombo.activated.connect(self.switchPage)
+        # Create the stacked layout
+        self.stackedLayout = QStackedLayout()
+        # Create the first page
+        self.page1 = QWidget()
+        self.page1Layout = QFormLayout()
+        self.page1Layout.addRow("Name:", QLineEdit())
+        self.page1Layout.addRow("Address:", QLineEdit())
+        self.page1.setLayout(self.page1Layout)
+        self.stackedLayout.addWidget(self.page1)
+        # Create the second page
+        self.page2 = QWidget()
+        self.page2Layout = QFormLayout()
+        self.page2Layout.addRow("Job:", QLineEdit())
+        self.page2Layout.addRow("Department:", QLineEdit())
+        self.page2.setLayout(self.page2Layout)
+        self.stackedLayout.addWidget(self.page2)
+        # Add the combo box and the stacked layout to the top-level layout
+        layout.addWidget(self.pageCombo)
+        layout.addLayout(self.stackedLayout)
         
         # create video window
         #self.video_gui_window = QtGui.QWindow()
+
         
         self.video = QtWidgets.QWidget()
         self.video.setStyleSheet("""background-color: black;""")
