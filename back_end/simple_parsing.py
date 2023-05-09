06d05e0317ea2ca1ddebe1d46207c021377ded49
LZandbergen <lzandbergen@outlook.com>
2023-05-09 14:12:48 +0200

    testing

--- back_end/simple_parsing.py
+++ back_end/simple_parsing.py
@@ -56,15 +56,21 @@ def is_word_spoken(subs, word):
         return False
     else:
         strings = [sub.text for sub in subs]
-    print(any(word in text for text in strings))
+    if any(word in text for text in strings) != any(word in sub.text for sub in subs):
+        print(f"word: {word}", strings)
+        print(any(word in text for text in strings))
+        print(any(word in sub.text for sub in subs))
     return any(word in text for text in strings)
 
-nlp, translator = load_parser()
-en_subs, sp_subs = load_subtitles()
-
-for en_sub in en_subs[:50]:
+def parse_subtitle_text(sub):
     expression = re.compile("[\(\<].*?[\)\>]")
-    en_text = expression.sub("", en_sub.text)
+    return expression.sub("", sub.text)
+
+nlp, translator = load_parser()
+sp_subs, en_subs = load_subtitles()
+
+for en_sub in en_subs:
+    en_text = parse_subtitle_text(en_sub)
     en_doc = nlp(en_text)
 
     # Looks for English words that are nouns and creates data to work with
@@ -86,8 +92,7 @@ for en_sub in en_subs[:50]:
 
             # Adds a word with its frequency to the dictionary if it is an actual translation
             if (en_word_str != "unknown" and len(sp_word) > 1 and
-                    en_word_str != sp_word and " " not in sp_word):
-                is_word_spoken(subs, en_word_str)
+                    en_word_str != sp_word and " " not in sp_word and is_word_spoken(subs, sp_word)):
                 word_frequency = get_word_frequency(sp_word, 'es')
                 word_freq_dict[sp_word] = word_frequency
                 noun_translations.append((sp_word, en_word))
